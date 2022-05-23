320765d9f705d9be50fcf576225819475c97a782
razumolla <rajuahmed9344@gmail.com>
2022-05-23 20:58:02 +0600

    aggreate system add for slot

--- index.js
+++ index.js
@@ -19,13 +19,72 @@ async function run() {
         await client.connect();
         console.log("Database Connected");
         const serviceCollection = client.db("doctors_portal").collection("services");
+        const bookingCollection = client.db("doctors_portal").collection("booking");
 
-        // get data from server
+        // get appointment services data from server
         app.get('/services', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        })
+
+
+        // Warning: This is not the proper way to query multiple collection. 
+        // After learning more about mongodb. use aggregate, lookup, pipeline, match, group
+        app.get('/available', async (req, res) => {
+            const date = req.query.date;
+
+            // step 1:  get all services
+            const services = await serviceCollection.find().toArray();
+
+            // step 2: get the booking of that day. output: [{}, {}, {}, {}, {}, {}]
+            const query = { date: date };
+            const bookings = await bookingCollection.find(query).toArray();
+
+            // step 3: for each service
+            services.forEach(service => {
+                // step 4: find bookings for that service. output: [{}, {}, {}, {}]
+                const serviceBookings = bookings.filter(book => book.treatment === service.name);
+                // step 5: select slots for the service Bookings: ['', '', '', '']
+                const bookedSlots = serviceBookings.map(book => book.slot);
+                // step 6: select those slots that are not in bookedSlots
+                const available = service.slots.filter(slot => !bookedSlots.includes(slot));
+                //step 7: set available to slots to make it easier 
+                service.slots = available;
+            });
+
+            res.send(services);
+        });
+
+
+        /**
+                   * API Naming convention
+                   * -------------------------
+                   * app.get('/booking) //get all bookings in this collection , or get more than  one or by filter
+                   * app.get('/booking/:id) //get a specific booking 
+                   * 
+                   * app.post('/booking) //add a new booking (create)
+                   * app.patch('/booking/:id) //specific one item (update)
+                   * app.delete('/booking/:id) //specific one item (delete)
+                   * 
+                  */
+
+
+
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = {
+                treatment: booking.treatment,
+                date: booking.date,
+                patient: booking.patient
+            }
+            const exists = await bookingCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, booking: exists })
+            }
+            const result = bookingCollection.insertOne(booking);
+            return res.send({ success: true, result });
         })
 
 
