584f9848d8c534c2464b0db5100d8361433e6d8f
razumolla <rajuahmed9344@gmail.com>
2022-05-23 20:59:05 +0600

    my appointment get data

--- index.js
+++ index.js
@@ -70,7 +70,12 @@ async function run() {
                    * 
                   */
 
-
+        app.get('/booking', async (req, res) => {
+            const patient = req.query.patient;
+            const query = { patient: patient };
+            const bookings = await bookingCollection.find(query).toArray();
+            res.send(bookings);
+        })
 
         app.post('/booking', async (req, res) => {
             const booking = req.body;
